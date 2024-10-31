7c82b31630625d8c650eced2ad8f01d3ff2576d4
Floris-V <fpvoogt@ziggo.nl>
2024-10-31 13:35:25 +0100

    Removed previous changes and started following a tutorial

---
+++ AIML-Project/Assets/Soccer/Scripts/Sensors/DetectableTargetManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectableTargetManager : MonoBehaviour
+{
+    public static DetectableTargetManager Instance { get; private set; } = null;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
