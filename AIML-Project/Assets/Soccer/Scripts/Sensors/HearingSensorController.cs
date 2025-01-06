8d1cae046c09ee0ecd73d6074024da08ec76805f
Floris-V <fpvoogt@ziggo.nl>
2025-01-06 18:22:27 +0100

    Refactoring
    Refactored the HearingSensorController.cs and RaycastPerceptionRotatingSensor.cs to standardise code style. Mainly turned numbers into variables.

--- AIML-Project/Assets/Soccer/Scripts/Sensors/HearingSensorController.cs
+++ AIML-Project/Assets/Soccer/Scripts/Sensors/HearingSensorController.cs
@@ -9,9 +9,18 @@ public class HearingSensorController : MonoBehaviour
     private Observation[] observations;
     private List<Transform> collisions = new List<Transform>();
     private Transform agentTransform;
+
+    private int observNmbr = 4;
+    private float minVelMag = 0.01f;
+
+    private String tagBall = "ball";
+    private String tagBA = "blueAgent";
+    private String tagPA = "purpleAgent";
+
+    //Create list of observations
     private void Start()
     {
-        observations = new Observation[4];
+        observations = new Observation[observNmbr];
         for (int i = 0; i < observations.Length; i++)
         {
             observations[i] = new Observation();
@@ -19,9 +28,10 @@ public class HearingSensorController : MonoBehaviour
         agentTransform=transform.parent;
     }
     
+    //When something collides with the object, add to list of collisions
     private void OnTriggerStay(Collider other)
     {
-        if(!(other.tag=="ball"|| other.tag=="blueAgent"|| other.tag=="purpleAgent"))
+        if(!(other.tag==tagBall || other.tag==tagBA || other.tag==tagPA))
             return;
         if (other.transform == agentTransform)
             return;
@@ -29,9 +39,10 @@ public class HearingSensorController : MonoBehaviour
             collisions.Add(other.transform);
     }
 
+    //When no longer colliding, remove from list of collisions
     private void OnTriggerExit(Collider other)
     {
-        if(!(other.tag=="ball"|| other.tag=="blueAgent"|| other.tag=="purpleAgent"))
+        if(!(other.tag==tagBall || other.tag==tagBA || other.tag==tagPA))
             return;
         if (other.transform == agentTransform)
             return;
@@ -39,6 +50,7 @@ public class HearingSensorController : MonoBehaviour
             collisions.Remove(other.transform);
     }
 
+    //Adds all observations to a sensor
     public void AddObservations(VectorSensor sensor)
     {
         DetectCollisions();
@@ -52,12 +64,12 @@ public class HearingSensorController : MonoBehaviour
 
     private void DetectCollisions()
     {
-        observations = new Observation[4];
+        observations = new Observation[observNmbr];
         int i = 0;
         foreach (Transform t in collisions)
         {
             Rigidbody rb = t.GetComponent<Rigidbody>();
-            if (rb == null || rb.velocity.magnitude<0.01f) continue;
+            if (rb == null || rb.velocity.magnitude<minVelMag) continue;
             if (i < observations.Length)
                 observations[i] = new Observation(transform,t);
             i++;
@@ -81,22 +93,25 @@ internal class Observation
     {
     }
 
-    
     public Observation(Transform agent, Transform observed)
     {
         isObserved = true;
         float distance = Vector3.Distance(agent.position, observed.position);
-        //y=\frac{1}{1+e^{-\left(x-c\right)}}\cdot0.6 (desmos)
-        //radius = 1/(1+ e^(2.5-distance))*0.6 * max_radius
-        radius = MAX_RADIUS * 0.6f*(float)(1/(1+Math.Pow(Math.E,(2.5-distance))));
+        CalcRadius(distance);
         float randomX = UnityEngine.Random.Range(-radius, radius);
         float randomZ = UnityEngine.Random.Range(-radius, radius);
-        position = new Vector3(observed.position.x + randomX, observed.position.y, observed.position.z + randomZ);
-        
+        position = new Vector3(observed.position.x + randomX, observed.position.y, observed.position.z + randomZ);   
+    }
+
+    private float CalcRadius(float distance)
+    {
+        //y=\frac{1}{1+e^{-\left(x-c\right)}}\cdot0.6 (desmos)
+        //radius = 1/(1+ e^(2.5-distance))*0.6 * max_radius
+        return radius = MAX_RADIUS * 0.6f * (float)(1 / (1 + Math.Pow(Math.E, (2.5 - distance))));
     }
 
     public override string ToString()
     {
-        return "IsObserved: " + isObserved + " Ppsition: (" + position + ")";
+        return "IsObserved: " + isObserved + " Position: (" + position + ")";
     }
 }
